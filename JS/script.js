65bfa3853f9de48ef17c2189df243de019409450
evsik <evsevsikov@gmail.com>
2021-01-23 13:05:42 +0300

    changed JSON

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,18 @@
 //ПЕРЕДЕЛАТЬ НА JSON
 
 
-// let IMAGES = ["../images/tinified/1.jpg", "../images/tinified/2.jpg", "../images/tinified/3.jpg",
-//     "../images/tinified/4.jpg", "../images/tinified/5.jpg", "../images/tinified/6.jpg", "../images/tinified/7.jpg",
+// let IMAGES = ["../images/tinified/6.jpg", "../images/tinified/7.jpg",
 //     "../images/tinified/8.jpg", "../images/tinified/9.jpg", "../images/tinified/10.jpg", "../images/tinified/11.jpg",
 //     "../images/tinified/12.jpg", "../images/tinified/13.jpg", "../images/tinified/14.jpg", "../images/tinified/15.jpg",
 //     "../images/tinified/16.jpg", "../images/tinified/17.jpg", "../images/tinified/18.jpg", "../images/tinified/19.jpg",
 //     "../images/tinified/20.jpg"]
 //
-// let CITY = ["Moscow", "Budapest", "Budapest", "Budapest", "Budapest", "Budapest",
+// let CITY = ["Budapest",
 //     "Montenegro", "Paris", "Prague", "Budapest", "Moscow", "Budapest",
 //     "Budapest", "Budapest", "Moscow", "Budapest", "Paris", "Paris", "Paris",
 //     "Paris"]
 //
-// let DATES = ["21.11.2020", "8.08.2018", "8.08.2018", "7.08.2018", "7.08.2018",
-//     "7.08.2018", "6.08.2019", "28.03.2019", "4.06.2019", "7.08.2018",
+// let DATES = ["7.08.2018", "6.08.2019", "28.03.2019", "4.06.2019", "7.08.2018",
 //     "25.08.2019", "7.08.2018", "8.08.2018", "6.08.2018", "3.10.2020",
 //     "7.08.2018", "27.03.2019", "28.03.2019", "28.03.2019", "29.03.2019"]
 
@@ -47,7 +45,7 @@ function createBlock(index) {
 let gallery = {
     items: [],
     container: ".contentUl",
-    catalogUrl: 'https://raw.githubusercontent.com/evsik/Landing-with-video/master/photosData.json',
+    catalogUrl: ' https://raw.githubusercontent.com/evsik/Landing-with-video/master/photosData.json',
 
     init() {
         this.items = []
@@ -65,6 +63,7 @@ let gallery = {
             })
     },
     _fetchItems() {
+
         // let length = IMAGES.length;
         //
         // for (let i = 0; i < length; i++) {
