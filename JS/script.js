f87e1519841a0c2524926991f69a1c4dfed4da41
evsik <evsevsikov@gmail.com>
2021-01-23 13:24:44 +0300

    changed JSON

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,3 @@
-//ПЕРЕДЕЛАТЬ НА JSON
-
-
-// let IMAGES = ["../images/tinified/6.jpg", "../images/tinified/7.jpg",
-//     "../images/tinified/8.jpg", "../images/tinified/9.jpg", "../images/tinified/10.jpg", "../images/tinified/11.jpg",
-//     "../images/tinified/12.jpg", "../images/tinified/13.jpg", "../images/tinified/14.jpg", "../images/tinified/15.jpg",
-//     "../images/tinified/16.jpg", "../images/tinified/17.jpg", "../images/tinified/18.jpg", "../images/tinified/19.jpg",
-//     "../images/tinified/20.jpg"]
-//
-// let CITY = ["Budapest",
-//     "Montenegro", "Paris", "Prague", "Budapest", "Moscow", "Budapest",
-//     "Budapest", "Budapest", "Moscow", "Budapest", "Paris", "Paris", "Paris",
-//     "Paris"]
-//
-// let DATES = ["7.08.2018", "6.08.2019", "28.03.2019", "4.06.2019", "7.08.2018",
-//     "25.08.2019", "7.08.2018", "8.08.2018", "6.08.2018", "3.10.2020",
-//     "7.08.2018", "27.03.2019", "28.03.2019", "28.03.2019", "29.03.2019"]
-
 function createBlock(index) {
     return {
         // blockImg: IMAGES[index],
